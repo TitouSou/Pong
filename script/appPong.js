e30e9a075bc69108d7a39de8186aab9c6e34f44c
TitouSou <titisou14@outlook.fr>
2025-09-24 16:57:47 +0200

    barre + ecran fin tout fonctionnel

--- script/appPong.js
+++ script/appPong.js
@@ -3,48 +3,87 @@ const ctx = canvas.getContext("2d");
 
 let startTime = Date.now();
 let score = (Date.now() - startTime) / 1000;
-let speedCoef = 1;
+let gameRunning = false;
 let rafId;
-let xSpeed = 2;
-let ySpeed = 2;
+
 
 let ball = {
     x : 50,
     y : 50,
-    radius : 10
+    xSpeed : 1,
+    ySpeed : -1,
+    radius : 10,
+    speedCoef : 1
+}
+
+let barre = {
+    lenght : 70,
+    x : canvas.width / 2 - 35,
+    y : canvas.height - 20,
+    speed : 3,
+    movingLeft : false,
+    movingRight : false
 }
 
 function startGame() {
+    if (rafId) cancelAnimationFrame(rafId);
+    gameRunning = true;
     startTime = Date.now();
     score = 0;
-    speedCoef = 1;
+
+    ball.x = Math.random() * (canvas.width - 2 * ball.radius) + ball.radius;
+    ball.y = Math.random() * (canvas.height - 2 * ball.radius - canvas.height / 10) + ball.radius;
+    ball.xSpeed = 1;
+    ball.ySpeed = -1;
+    ball.speedCoef = 1;
+
     update();
+    loop();
     
 }
 
 function update() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
     score = Math.floor(( Date.now() - startTime ) / 1000);
     console.log(score);
     document.getElementById("score").innerHTML = score;
 
-    ball.x += xSpeed;
-    ball.y += ySpeed;
-    if (ball.x > canvas.width - ball.radius) xSpeed = -xSpeed;
-    if (ball.x < ball.radius) xSpeed = -xSpeed;
-    if (ball.y > canvas.height - ball.radius) ySpeed = -ySpeed;
-    if (ball.y < ball.radius) ySpeed = -ySpeed;
-    
+    ball.x += ball.xSpeed;
+    ball.y += ball.ySpeed;
+    if (ball.x > canvas.width - ball.radius) {
+        ball.xSpeed = -ball.xSpeed * ball.speedCoef;
+        if (ball.speedCoef < 5) ball.speedCoef += 0.02;
+    }
+    if (ball.x < ball.radius) {
+        ball.xSpeed = -ball.xSpeed * ball.speedCoef;
+        if (ball.speedCoef < 5) ball.speedCoef += 0.02;
+    }
+    if (ball.y < ball.radius) {
+        ball.ySpeed = -ball.ySpeed * ball.speedCoef;
+        if (ball.speedCoef < 5) ball.speedCoef += 0.02;
+    }
+    if (ball.y + ball.radius > barre.y && ball.y + ball.radius < barre.y + 5 && ball.x > barre.x && ball.x < barre.x + barre.lenght) {
+        ball.ySpeed = -ball.ySpeed * ball.speedCoef;
+        if (ball.speedCoef < 5) ball.speedCoef += 0.02;
+    }
+    if (ball.y > canvas.height - ball.radius) {
+        loseScreen();
+        gameRunning = false;
+    }
+
+    if (barre.movingLeft && barre.x > 0) barre.x -= barre.speed;
+    if (barre.movingRight && barre.x + barre.lenght < canvas.width) barre.x += barre.speed; 
     
 }
 
 function loop() {
-    update();
+    if (gameRunning) update();
     drawBall();
+    drawBarre();
     rafId = requestAnimationFrame(loop);
 }
 
 function drawBall() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height); // efface l'écran
     ctx.fillStyle = "blue";
     //dessin de la balle
     ctx.beginPath();
@@ -53,14 +92,64 @@ function drawBall() {
     ctx.closePath();
 }
 
+function drawBarre() {
+    ctx.fillStyle = "red";
+    // dessin de la barre
+    ctx.beginPath();
+    ctx.fillRect(barre.x, barre.y, barre.lenght, 5);
+    ctx.fill();
+    ctx.closePath();
+}
+
+function loseScreen() {
+    ctx.fillStyle = "red";
+    ctx.textAlign = "center";
+    ctx.font = "40px arial";
+    ctx.fillText("YOU LOSE", canvas.width / 2, canvas.height / 2);
+}
+
 var reset=document.getElementById("reset");
 reset.addEventListener("click", function() {
     startGame();
 });
 
-var scoreLabel=document.getElementById("score");
-scoreLabel.addEventListener("click", function() {
-    update();
+var left=document.getElementById("left");
+left.addEventListener("click", function() {
+    barre.x -= barre.speed;
 });
 
-loop();
+var right=document.getElementById("right");
+right.addEventListener("click", function() {
+    barre.x += barre.speed;
+});
+
+document.addEventListener("keydown", (e) => {
+    switch (e.key) {
+        case "ArrowLeft":
+            barre.movingLeft = true; 
+            break;
+
+        case "ArrowRight":
+            barre.movingRight = true;
+            break;
+        case "Space":
+            startGame();
+        default:
+            break;
+    }
+});
+
+document.addEventListener("keyup", (e) => {
+    switch (e.key) {
+        case "ArrowLeft":
+            barre.movingLeft = false; 
+            break;
+
+        case "ArrowRight":
+            barre.movingRight = false;
+            break;
+
+        default:
+            break;
+    }
+});
